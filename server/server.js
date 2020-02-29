3a12ccaee19465509d3ad6349e35ff2ed6aa9578
Matthew McPhillips <matthewcmcphillips@gmail.com>
2020-02-29 15:18:02 -0800

    Implement server to return mock data on componentDidMount

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 3333;
+const app = express();
+const mockData = require('./mockData.js');
+
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../client/dist/`));
+
+app.get('/api/movies', (req, res) => {
+  res.send(mockData);
+});
+
+app.listen(port, () => console.log('listening on port:' + port))
